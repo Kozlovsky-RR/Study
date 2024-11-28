507d21bd811f3a0dc7649c65646b21525f72ebe6
Ruslan Kozlovsky <kenny32534@gmail.com>
2024-11-28 23:28:59 +0400

    add text file, fixed some moments

--- beginner.py
+++ beginner.py
@@ -1,21 +1,26 @@
-'''Задача 1: Реверсирование строки
-Напиши функцию, которая принимает строку и возвращает её, но со словами в обратном порядке
-Пример:'''
+from collections import Counter
+
+
+"""Задача 1: Реверсирование строки"""
+
 
 def reverse_words(s: str) -> str:
+    """функция, которая принимает строку и возвращает её, но со словами в обратном порядке"""
     return ' '.join(s.split()[::-1])
+
 
 s = "Hello world from Python"
 print(reverse_words(s))
+'''Python from world Hello'''
 
-'''Задача 2: Максимальная сумма подмассива
-Напиши функцию, которая принимает список чисел и возвращает максимальную сумму подмассива
-Пример:'''
+
+'''Задача 2: Максимальная сумма подмассива'''
+
 
 def max_subarray_sum(nums: list) -> int:
+    """функция, которая принимает список чисел и возвращает максимальную сумму подмассива"""
     max_sum = 0
     current_sum = 0
-
 
     for i in range(len(nums)):
         current_sum = current_sum + nums[i]
@@ -26,34 +31,28 @@ def max_subarray_sum(nums: list) -> int:
         elif current_sum > max_sum:
             max_sum = current_sum
 
-
     return max_sum
 
-nums = [-2,1,-3,4,-1,2,1,-5,4]
+
+nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 print(max_subarray_sum(nums))
 
-'''Задача 3: Частота символов
-Напиши функцию, которая принимает строку и возвращает словарь, где ключами являются символы, а значениями — их частота в строке
-Пример:'''
+'''Задача 3: Частота символов'''
+
 
 def char_frequency(s: str) -> dict:
-    my_dict = {}
+    return dict(Counter(s))
 
-    for i in s:
-        my_dict.setdefault(i, 0)
-        my_dict[i] += 1
-
-    return my_dict
 
 s = "abracadabra"
 print(char_frequency(s))
 
-'''Задача 4: Проверка анаграммы
-Напиши функцию, которая принимает две строки и проверяет, являются ли они анаграммами (т.е. состоят ли они из одних и тех же символов в одинаковом количестве)
-Пример:'''
+'''Задача 4: Проверка анаграммы'''
+
 
 def are_anagrams(s1: str, s2: str) -> bool:
-    return sorted(s1) == sorted(s2)
+    """функция, которая принимает две строки и проверяет, являются ли они анаграммами"""
+    return Counter(s1) == Counter(s2)
 
 
 s1 = "listen"
